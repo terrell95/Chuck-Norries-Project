37451b30ebb5b1917ececc34e53ceaef84317bd3
Terrell Oliver <terrelloliver95@gmail.com>
2019-03-29 16:39:10 -0700

    Added chuck norries project

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getWebResponse("https://api.chucknorris.io/jokes/random"));
+
+            Console.ReadLine();
+        }
+
+        static string getWebResponse(string url)
+        {
+            WebClient webClient = new WebClient();
+            string apiResponse = webClient.DownloadString(url);
+            string joke = JObject.Parse(apiResponse)["value"].ToString();
+            return joke;           
+        }      
+    }
+}
